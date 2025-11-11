9f61561c9c13a583253f137f65e4510e7cd053ad
kalachuuchiiii <parissrowlet@gmail.com>
2025-11-12 00:41:27 +0800

    fixed bugs, fixed ghost image

--- src/data/constants/projects.ts
+++ src/data/constants/projects.ts
@@ -1,4 +1,6 @@
 import type { Project } from "../../types/project";
+import inq from '/inquestia.png';
+import telltl from '/telltale.png';
 
 
 export const projects: Project[] = [
@@ -7,7 +9,7 @@ export const projects: Project[] = [
     type: "Full Stack Application",
   
     link: 'https://inquestia-ask.vercel.app',
-    img: '/public/inquestia.png',
+    img: inq,
     repoLink: 'https://github.com/kalachuuchiiii/inquestia',
     description:
       "A full-stack survey platform built to bridge the gap between researchers and participants — powered by React, Node.js, and MongoDB. It rewards respondents for participation while helping researchers gather meaningful data efficiently.",
@@ -18,7 +20,7 @@ export const projects: Project[] = [
 
     link: 'https://thetelltale.vercel.app',
      repoLink: 'https://github.com/kalachuuchiiii/telltale-front',
-    img: '/public/telltale.png',
+    img: telltl,
     description:
       "This app is a full-stack platform where people share unsent messages, creating a collective archive of personal expression.",
   },
